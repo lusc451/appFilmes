0a7c3e8cc01de2251a8bf21b1b5213275fa3ed84
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-06-13 07:40:04 -0300

    App filmes

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "./src/screens/LoginScreen";
+import CadastroScreen from "./src/screens/CadastroScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import DetalhesFilmeScreen from "./src/screens/DetalhesFilmeScreen";
+import { StatusBar } from "react-native";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar backgroundColor="#000" barStyle="light-content" />
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{
+            title: "LOGIN DO USUÁRIO",
+            headerTitleAlign: "center",
+            headerStyle: { backgroundColor: "#B81D24" },
+            headerTintColor: "#fff",
+            headerTitleStyle: { fontWeight: "bold" },
+          }}
+        />
+        <Stack.Screen
+          name="Cadastro"
+          component={CadastroScreen}
+          options={{
+            title: "CADASTRO DE USUÁRIO",
+            headerTitleAlign: "center",
+            headerStyle: { backgroundColor: "#B81D24" },
+            headerTintColor: "#fff",
+            headerTitleStyle: { fontWeight: "bold" },
+          }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: "FILMES POPULARES",
+            headerLeft: null,
+            headerTitleAlign: "center",
+            headerStyle: { backgroundColor: "#B81D24" },
+            headerTintColor: "#fff",
+            headerTitleStyle: { fontWeight: "bold" },
+          }}
+        />
+        <Stack.Screen
+          name="DetalhesFilme"
+          component={DetalhesFilmeScreen}
+          options={{
+            title: "DETALHES DO FILME",
+            headerTitleAlign: "center",
+            headerStyle: { backgroundColor: "#B81D24" },
+            headerTintColor: "#fff",
+            headerTitleStyle: { fontWeight: "bold" },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
